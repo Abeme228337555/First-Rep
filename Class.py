4df596f9f62cc7ef8581720ccc51a13446b4d1c9
Abeme <musiienko.andrii@gmail.com>
2022-05-08 12:03:01 +0300

    Initial Commit

--- Class.py
+++ Class.py
@@ -1,1 +1,26 @@
-print(0)
+class Car:
+    def __init__(self, fuel, maxspeed):
+        self.fuel = fuel
+        self.maxspeed = maxspeed
+    def refuel(self, amount):
+        self.fuel += amount
+    def drive(self):
+        if self.fuel > 0:
+            print('Driving')
+            self.fuel -= 1
+        else:
+            print("No fuel")
+polo = Car(10, 140)
+print(polo.fuel)
+print(polo.maxspeed)
+ferrari = Car(15, 290)
+print(ferrari.fuel)
+print(ferrari.maxspeed)
+print('-------------------')
+print(polo.fuel)
+polo.refuel(10)
+print(polo.fuel)
+print('-------------------')
+print(ferrari.fuel)
+ferrari.drive()
+print(ferrari.fuel)
